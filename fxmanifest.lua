f1c4410f1c97ebb9a8ed26a23f1f1dd0f11b4b5d
Puntherline <53500972+Puntherline@users.noreply.github.com>
2020-09-04 16:09:10 +0200

    Some updates
    Updated the fx_version from adamant to cerulean and the script version is now 1.1.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 game 'gta5'
 
 author 'Puntherline'
 description 'Advanced whitelist / password script.'
-version '1.0'
+version '1.1'
 
 server_script 'server.lua'
